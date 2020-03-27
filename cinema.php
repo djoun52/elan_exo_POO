3e28ccddcb42094cee8cf96309e6a08160bc0741
djoun52 <33383124+djoun52@users.noreply.github.com>
2020-03-27 16:48:23 +0100

    fin de journé

---
+++ cinema.php
@@ -0,0 +1,12 @@
+<a href=" ./index.php" title="index"> index</a>
+<br>
+
+
+<?php
+
+
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <h1>
+            exo Elan
+        </h1>
+    </header>
+
+
+    <main>
+        <div>
+            <a href=" ./banque.php" title="exo1">exo banque</a>
+        </div>
+        <hr>
+
+        <div>
+            <a href=" ./cinema.php" title="exo2">exo cinema</a>
+        </div>
+        <hr>
+        </main>
+
+</body>
+
+</html>
