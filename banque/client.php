01151d7cb2f9cded82c10cb6a564224d743a6ba3
djoun52 <33383124+djoun52@users.noreply.github.com>
2020-03-31 14:07:29 +0200

    finit pas corrigé

---
+++ banque/client.php
@@ -0,0 +1,83 @@
+<?php
+class Titulaire
+{
+    // Properties
+    
+    private $nom ;
+    private $prenom ;
+    private $dateDeNaissance ;
+    private $ville ;
+    public $comptes = [];
+  
+
+    public function __construct($c_nom, $c_prenom, $c_dateDeNaissance,$c_ville)
+    {
+        $this->nom = $c_nom;
+        $this->prenom = $c_prenom;
+        $this->dateDeNaissance = $c_dateDeNaissance;
+        $this->ville = $c_ville ;
+      
+    
+    }
+        // Methods
+        public function getNom(){
+            return $this->nom;
+        }
+        public function setNom($p_nom){
+            $this->nom = $p_nom;
+        }
+        public function getPrenom(){
+            return $this->prenom;
+        }
+        public function setPrenom($p_prenom){
+            $this->prenom = $p_prenom;
+        }
+        public function getDateDeNaissance(){
+            return $this->dateDeNaissance;
+        }
+        public function setDateDeNaissance($p_dateDeNaissance){
+            $this->dateDeNaissance = $p_dateDeNaissance;
+        }
+        public function getAge(){
+            $date= new DateTime();
+            $date1= new DateTime ($this->getDateDeNaissance());
+            $diff=date_diff($date1,$date);
+            return $diff->format(" %y ans");
+        }
+
+        public function getVille(){
+            return $this->ville;
+        }
+        public function setVille($p_ville){
+            $this->ville = $p_ville;
+        }
+
+        public function addCompts($param){
+            $this->comptes = $param;
+        }
+        public function getCompts(){
+            return $this->comptes;
+        }
+        public function getComptesInfo(){
+            $s="";
+            $s.= "<br> info comptes <br> ************** <br>";
+            foreach ($this->comptes as $key => $value){
+                
+                $s.= $value->infoComptC() ;
+            }
+            return $s;
+        }
+        
+    
+        function infoTitulaire(){
+           
+            $s="";
+            $s.= "<br> info <br> ************** <br>";
+            $s.= "le nom du titulaire : " . $this->getNom() ."<br>";
+            $s.= "le prénom du titulaire :" . $this->getPrenom() ."<br>" ;
+            $s.= "Le titulaire à " . $this->getAge() . "<br>"; 
+            $s.= "Le titulaire vie à " . $this->getVille() . "<br>"; 
+            $s.= $this->getComptesInfo();
+            return  $s;
+        }
+}
---
+++ banque/compts.php
@@ -0,0 +1,128 @@
+<?php
+class Compte
+{
+    // Properties
+    
+    protected  $libelle;
+    protected  $soldeInitial;
+    protected  $deviseMonétaire;
+    protected  $numero;
+    public      $client;
+  
+
+    public function __construct(string $c_libelle,int  $c_soldeInitial,string $c_deviseMonétaire,$c_client,$c_numero)
+    {
+        is_string($c_libelle) ? $this->libelle = $c_libelle : NULL;
+        is_int($c_soldeInitial) ? $this->soldeInitial = $c_soldeInitial : NULL;
+        is_string($c_deviseMonétaire) ?$this->deviseMonétaire = $c_deviseMonétaire : NULL ;
+        is_object($c_client) ? $this->client = $c_client : NULL;
+        is_int($c_numero) ? $this->numero = $c_numero : NULL;
+        $this->client = $c_client;
+     
+    }
+    // Methods
+    public function getLibelle()
+    {
+        return $this->libelle;
+    }
+    public function setLibelle($p_libelle)
+    {
+        $this->libelle = $p_libelle;
+    }
+    public function getSoldeInitial()
+    {
+        return $this->soldeInitial;
+    }
+    public function setSoldeInitial($p_soldeInitial)
+    {
+        $this->marqusoldeInitiale = $p_soldeInitial;
+    }
+    public function getDeviseMonétaire()
+    {
+        return $this->deviseMonétaire;
+    }
+    public function setDeviseMonétaire($p_deviseMonétaire)
+    {
+        $this->deviseMonétaire = $p_deviseMonétaire;
+    }
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+    public function setNumero($p_numero)
+    {
+        $this->numero = $p_numero;
+    }
+    public function getClientNom()
+    {
+        $s = "";
+        $c= $this->client;
+        $s.= $c->getNom();
+        return $s;
+    }
+    public function getClientPrenom()
+    {
+        $s = "";
+        $c= $this->client;
+        $s.= $c->getPrenom();
+        return $s;
+    }
+    public function getClientage()
+    {
+        $s = "";
+        $c= $this->client;
+        $s.= $c->getAge();
+        return $s;
+
+    }
+    public function getClientInfo(){
+        $s ="";
+        $s.= " " . $this->getClientNom() . " " . $this->getClientPrenom()  ;
+        return $s;
+    }
+
+
+
+
+    public function créditer ($montant)
+    {
+        return $this->soldeInitial +=$montant;
+    }
+    public function débiter ($montant)
+    {
+        return $this->soldeInitial -=$montant;
+    }
+
+    public function virement($virement , $comptecredi ){
+ 
+       $s=  $this->débiter($virement);
+       $s.=$comptecredi->créditer($virement);
+       return $s;
+    }
+    
+    function infoCompt(){
+        {   
+   
+            $s="";
+            $s.= "<br> info <br> ************** <br>";
+            $s.= "Le numéro du compte : " .$this->getNumero() . "<br>";
+            $s.= "le libellé du compte : " . $this->getLibelle() ."<br>";
+            $s.= "le solde du compte :" . $this->getSoldeInitial() ."<br>" ;
+            $s.= "La devise du compte : " .$this->getDeviseMonétaire() . "<br>";          
+            $s.= "le nom et prenom du titulaire" . $this->getClientInfo() ."<br><br>";
+            return  $s;
+        }
+    }
+    function infoComptC(){
+        {   
+   
+            $s="";
+            $s.= "Le numéro du compte : " .$this->getNumero() . "<br>";
+            $s.= "le libellé du compte : " . $this->getLibelle() ."<br>";
+            $s.= "le solde du compte :" . $this->getSoldeInitial() ."<br>" ;
+            $s.= "La devise du compte : " .$this->getDeviseMonétaire() . "<br><br>";
+            return  $s;
+        }
+    }
+ 
+}
